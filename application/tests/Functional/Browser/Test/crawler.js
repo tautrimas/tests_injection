62281a20bf554c5e52ab7705108d3494b4c94c7b
Tautrimas Pajarskas <tautrimas@tautrimas.lt>
2014-08-24 22:48:05 +0300

    Use parameters file in CasperJS

--- application/tests/Functional/Browser/Test/crawler.js
+++ application/tests/Functional/Browser/Test/crawler.js
@@ -1,13 +1,15 @@
+require('../parameters');
+
 casper.test.begin('Url submitting', 2, function suite(test) {
     var slug = Math.floor((Math.random() * 1000000) + 1);
     var url = 'http://www.random.org/integers/?num=100&min=1&max=' + slug + '&col=5&base=10&format=html&rnd=new';
 
-    casper.start('http://workshop-revive.local/crawler/crawl', function () {
+    casper.start(document.app.parameters.base_url + '/crawler/crawl', function () {
         test.assertExists('form', 'Form must exist');
         this.fill('form', {url: url}, true);
     });
 
-    casper.thenOpen('http://workshop-revive.local/crawler/index', function () {
+    casper.thenOpen(document.app.parameters.base_url + '/crawler/index', function () {
         test.assertExists('.page-list a[href="' + url + '"]', 'Link must exist');
     });
 
---
+++ application/tests/Functional/Browser/parameters.example.js
@@ -0,0 +1,5 @@
+document.app = document.app || {};
+
+document.app.parameters = {
+    'base_url': 'http://localhost:8000'
+};
